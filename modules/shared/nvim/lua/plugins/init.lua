37cc520967ce9fc9fcb3184c1cf8d1b5d9cf5fba
andle <andrew.le197@gmail.com>
2024-03-13 11:01:29 -0400

    feat: dap for nvim

--- modules/shared/nvim/lua/plugins/init.lua
+++ modules/shared/nvim/lua/plugins/init.lua
@@ -283,44 +283,21 @@ local default_plugins = {
         require("nvim-surround").setup()
     end
   },
+  -- {
+  --   'mrcjkb/rustaceanvim',
+  --   version = '^4', -- Recommended
+  --   config = function()
+  --     require "plugins.configs.rustaceanvim"
+  --   end,
+  --   ft = { 'rust' },
+  -- },
  {
     "mfussenegger/nvim-dap",
     ft = { 'rust' },
   },
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-  },
-  {
-    "towolf/vim-helm",
-    ft = 'helm'
-  },
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-  },
-  {
-    "towolf/vim-helm",
-    ft = 'helm'
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    config = function()
-      require "plugins.configs.neogit"
-    end,
-    event = "VeryLazy"
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup()
-    end
+ {
+    "sakhnik/nvim-gdb",
+    ft = { 'rust' },
   },
 }
 
